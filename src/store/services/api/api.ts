ab86389168862d49e771ceddc554dfb666f5d74a
Filippo Floramo <filifloramo@gmail.com>
2022-10-28 14:42:36 +0200

    change country code source

--- src/store/services/api/api.ts
+++ src/store/services/api/api.ts
@@ -4,10 +4,10 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const emissionAPI = createApi({
    reducerPath: 'emissionAPI',
-   baseQuery: fetchBaseQuery({ baseUrl: 'https://pkgstore.datahub.io/' }),
+   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.v2.emissions-api.org/api/v2/' }),
    endpoints: (build) => ({
       getEmissionCountries: build.query<any, void>({
-         query: () => `/core/country-list/data_json/data/8c458f2d15d9f2119654b29ede6e45b8/data_json.json`
+         query: () => `countries.json`
       }),
    }),
 });
