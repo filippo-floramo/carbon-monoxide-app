4290f3653a85817268b46947193b6478bc51a23f
Filippo Floramo <filifloramo@gmail.com>
2022-10-28 21:32:30 +0200

    add types

--- src/components/Modal/ModalSelect/ModalSelect.tsx
+++ src/components/Modal/ModalSelect/ModalSelect.tsx
@@ -4,13 +4,12 @@ import { useDispatch } from "react-redux";
 import { useGetEmissionCountriesQuery, } from "../../../store/services/api/api";
 import { addCountryCode } from "../../../store/features/modalSlice";
 
-
 interface DataTypes {
    label: string
    value: string
 }
 
-type SelectTypes = DataTypes | string | null
+type SelectTypes = DataTypes | null
 
 export default function ModalSelect() {
 
@@ -20,33 +19,33 @@ export default function ModalSelect() {
 
    if (isLoading) {
       return <div>Loading...</div>
-   };
+   }
+   if (!data) {
+      return <div>Data not found</div>
+   }
 
-   const filteredCountryCodes = Object.keys(data).filter((key) => {
+   const filteredCountryCodes: string[] = Object.keys(data || []).filter((key) => {
       return key.length <= 2
    });
 
-   const countryOptions: any = filteredCountryCodes.map((key: string) => {
-      return { label: data[key], value: key }
+   const countryOptions: DataTypes[] = filteredCountryCodes.map((key) => {
+      return { label: (data || [])[key], value: key }
    });
 
    return (
-      <>
-         <Select
-            name="country"
-            classNamePrefix="select"
-            className="country--select"
-            placeholder="Search"
-            defaultValue={" "}
-            onChange={(country: SelectTypes) => {
-               if (typeof (country) !== "string") {
-                  dispatch(addCountryCode(country?.value));
-               }
-            }}
-            isSearchable={true}
-            isClearable={true}
-            options={countryOptions}
-         />
-      </>
+      <Select
+         name="country"
+         classNamePrefix="select"
+         className="country--select"
+         placeholder="Search"
+         onChange={(country: SelectTypes) => {
+            if (country) {
+               dispatch(addCountryCode(country.value));
+            }
+         }}
+         isSearchable={true}
+         isClearable={true}
+         options={countryOptions}
+      />
    )
 }
--- src/store/services/api/api.ts
+++ src/store/services/api/api.ts
@@ -1,12 +1,14 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-
+interface CountryCodes {
+   [index: string] : string
+}
 
 export const emissionAPI = createApi({
    reducerPath: 'emissionAPI',
    baseQuery: fetchBaseQuery({ baseUrl: 'https://api.v2.emissions-api.org/api/v2/' }),
    endpoints: (build) => ({
-      getEmissionCountries: build.query<any, void>({
+      getEmissionCountries: build.query<CountryCodes, void>({
          query: () => `countries.json`
       }),
    }),
