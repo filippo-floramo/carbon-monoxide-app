1557e70493cd93a016526cfefb4db1db3e8f6e74
Filippo Floramo <filifloramo@gmail.com>
2022-11-11 17:40:11 +0100

    added modal conditional rendering

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ModalIndex from "../../components/Modal/ModalIndex/ModalIndex";
-
+import { useAtom } from "jotai";
+import { ModalOpen } from "../../atoms/atoms";
 
 export default function Home(): JSX.Element {
+
+   const [isModalOpen] = useAtom(ModalOpen);
+
    return (
       <>
          <div className="home">
             <h1>Hello Project</h1>
             <button>Click Me</button>
-            <ModalIndex />
+            {isModalOpen && <ModalIndex />}
          </div>
       </>
    )
