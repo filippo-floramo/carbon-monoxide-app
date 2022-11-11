4d412ab682c7ca8ac1d881a9623eea65d4c9de08
Filippo Floramo <filifloramo@gmail.com>
2022-11-11 17:41:36 +0100

    added modal state

--- src/atoms/atoms.tsx
+++ src/atoms/atoms.tsx
@@ -1,5 +1,8 @@
-import { atom } from "jotai";
+import { atom, useAtom } from "jotai";
 
-export const nation = atom<string | undefined>(" ")
+export const nation = atom<string | undefined>(" ");
+
+export const ModalOpen = atom<boolean>(true);
+
 
 
