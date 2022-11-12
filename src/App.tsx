21f530175043530119bad62404f133ed5609dd8e
Filippo Floramo <filifloramo@gmail.com>
2022-11-12 18:34:46 +0100

    added results page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Results from "./pages/Results/Results";
 import "./App.scss";
 
 function App(): JSX.Element {
-  
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/results" element={<Results />} />
     </Routes>
   )
 }
---
+++ src/pages/Results/Results.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import MainChart from "../../components/Charts/MainChart";
+
+
+
+
+export default function Results(): JSX.Element {
+
+
+   return (
+      <>
+         <div className="results">
+            <MainChart />
+         </div>
+      </>
+   )
+}
