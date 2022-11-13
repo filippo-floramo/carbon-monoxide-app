a2c57b1c657e5ca10f7f56cf4057cb356f872913
Filippo Floramo <filifloramo@gmail.com>
2022-11-13 16:00:10 +0100

    add custom hook to use atoms better

--- src/atoms/atoms.tsx
+++ src/atoms/atoms.tsx
@@ -1,8 +1,29 @@
 import { atom, useAtom } from "jotai";
 
-export const nation = atom<string | undefined>(" ");
 
-export const ModalOpen = atom<boolean>(true);
+
+//Declaring Atoms
+
+const modalOpen = atom<boolean>(false);
+
+const searchType = atom<boolean | null>(null);
+
+//Custom Hook to use atoms everywhere
+
+export default function useStateAtoms() {
+
+   const [isModalOpen, setIsModalOpen] = useAtom(modalOpen);
+
+   const [isCountrySearch, setIsCountrySearch] = useAtom(searchType)
+
+   return {
+      isModalOpen,
+      setIsModalOpen,
+      isCountrySearch,
+      setIsCountrySearch
+   }
+}
+
 
 
 
